f0939fd3240e10428183790463ea689c27b2fc6b
rucas <lucas.rondenet@gmail.com>
2018-02-23 11:07:47 -0800

    Go stuff...and more go stuff

--- go/bootstrap.sh
+++ go/bootstrap.sh
@@ -1,1 +1,14 @@
-go get -u github.com/nsf/gocode # needed for go autocomplete
+# needed for go autocomplete
+# 
+go get -u github.com/nsf/gocode 
+
+# Package manager
+#
+go get -u github.com/golang/dep/cmd/dep
+
+# Got to love the go tour
+#
+go get -u golang.org/x/tour/gotour
+
+go get -u github.com/motemen/gore
+go get -u github.com/k0kubun/pp
