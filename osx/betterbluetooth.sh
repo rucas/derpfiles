66c258db0128ba58ee31b5cf9e7832847f4ea6eb
rucas <lucas.rondenet@gmail.com>
2018-08-14 10:29:27 -0700

    fallbacks

--- osx/betterbluetooth.sh
+++ osx/betterbluetooth.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# force better codecs 
 sudo defaults write bluetoothaudiod "Enable AptX codec" -bool true
-
 sudo defaults write bluetoothaudiod "Enable AAC codec" -bool true
+
+# better defaults when better codecs fallback
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Max (editable)" 80
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" 80
+defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool (editable)" 80
+defaults write com.apple.BluetoothAudioAgent "Apple Initial Bitpool Min (editable)" 80
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool" 80
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Max" 80
+defaults write com.apple.BluetoothAudioAgent "Negotiated Bitpool Min" 80
