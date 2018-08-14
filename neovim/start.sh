47877d0543ced71050c5422963ea173b2ce4810a
rucas <lucas.rondenet@gmail.com>
2018-08-14 11:28:37 -0700

    update neovim to use pyenv with virtualenv

--- neovim/start.sh
+++ neovim/start.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-# needed for deoplete
-echo  "installing neovim python2 plugin..."
-pip2 install neovim
-echo  "installing neovim python3 plugin..."
-pip3 install neovim
+pyenv install 2.7.11
+pyenv install 3.4.4
+
+pyenv virtualenv 2.7.11 neovim2
+pyenv virtualenv 3.4.4 neovim3
+
+pyenv activate neovim2
+pip install neovim
+pyenv which python  # Note the path
+
+pyenv activate neovim3
+pip install neovim
+pyenv which python  # Note the path
