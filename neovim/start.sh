22c88b751a07395a65bbb458a8f152841dd3e348
rucas <lucas.rondenet@gmail.com>
2018-10-30 16:18:53 -0700

    3.5.6 mandatory for deoplete now

--- neovim/start.sh
+++ neovim/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 pyenv install 2.7.11
-pyenv install 3.4.4
+pyenv install 3.5.6
 
 pyenv virtualenv 2.7.11 neovim2
-pyenv virtualenv 3.4.4 neovim3
+pyenv virtualenv 3.5.6 neovim3
 
 pyenv activate neovim2
 pip install neovim
