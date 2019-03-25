6fb41a8fc3e2b5fcf68078de59d6546f68c8407b
rucas <lucas.rondenet@gmail.com>
2019-03-25 16:51:34 -0700

    ctop

--- docker/dockerfunc.sh
+++ docker/dockerfunc.sh
@@ -3,3 +3,9 @@
 htrace() {
   docker run --rm -it --name htrace.sh htrace.sh -u "$@"
 }
+
+ctop() {
+  docker run --rm -it --name=ctop \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    quay.io/vektorlab/ctop:latest
+}
