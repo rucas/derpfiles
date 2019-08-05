313f6acabd63ea8f8066e5a2002d35bfd541fd71
Lucas Rondenet <lucas.rondenet@gmail.com>
2019-08-05 15:15:12 -0700

    Update start.sh

--- neovim/start.sh
+++ neovim/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 pyenv install 2.7.11
-pyenv install 3.5.6
+pyenv install 3.6.1
 
 pyenv virtualenv 2.7.11 neovim2
-pyenv virtualenv 3.5.6 neovim3
+pyenv virtualenv 3.6.1 neovim3
 
 pyenv activate neovim2
 pip install pynvim
