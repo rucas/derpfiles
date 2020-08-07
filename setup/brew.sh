dc123a23bf931906a1e4468121272725920bbf75
Lucas Rondenet <lucas.rondenet@gmail.com>
2020-08-07 14:48:11 -0400

    Update brew.sh
    remove ruby dep

--- setup/brew.sh
+++ setup/brew.sh
@@ -4,5 +4,5 @@ if [ "/usr/local/bin/brew" = "$(which brew)" ]; then
     echo "Homebrew installed...skipping"
 else
     echo "Installing homebrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
