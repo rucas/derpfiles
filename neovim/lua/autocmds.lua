adc854b2d5ca480c6cf4db2f4890bd728cb4e92d
rucas <lucas.rondenet@gmail.com>
2021-12-06 17:21:42 -0800

    Jenkinsfile

--- neovim/lua/autocmds.lua
+++ neovim/lua/autocmds.lua
@@ -1,4 +1,7 @@
 -- TODO: figure out how to do grouping of auto commands
 
 -- YAML
-vim.cmd([[ autocmd FileType yaml setlocal ts=2 sts=2 sw=2 expandtab ]])
+vim.cmd([[ autocmd Filetype yaml setlocal ts=2 sts=2 sw=2 expandtab ]])
+
+-- Jenkinsfile
+vim.cmd([[ autocmd BufNewFile,BufRead Jenkinsfile set filetype=groovy ]])
