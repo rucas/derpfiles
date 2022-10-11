c453a669bb7a6e7ee64bcc066a57436674e3e7fb
rucas <lucas.rondenet@gmail.com>
2022-10-10 19:38:30 -0700

    git nix updates

--- flake.nix
+++ flake.nix
@@ -30,32 +30,9 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.lucas = import ./host/blkmrkt/home.nix;
+            home-manager.users.lucas = import ./hosts/blkmrkt/home.nix;
           }
         ];
       };
     };
-
-  #{
-  #  defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-  #  defaultPackage.x86_64-darwin = home-manager.defaultPackage.x86_64-darwin;
-
-  #  darwinConfigurations = {
-  #    blkmrkt-laptop = darwin.lib.darwinSystem {
-  #      system = "x86_64-darwin";
-  #      modules = [
-  #        ./configuration.nix
-  #        home-manager.darwinModules.home-manager
-  #        {
-  #          home-manager.useGlobalPkgs = true;
-  #          home-manager.useUserPackages = true;
-  #          home-manager.users.lucas = import ./home.nix;
-
-  #          # Optionally, use home-manager.extraSpecialArgs to pass
-  #          # arguments to home.nix
-  #        }
-  #      ];
-  #    };
-  #  };
-  #};
 }
--- modules/cli/git.nix
+++ modules/cli/git.nix
@@ -11,5 +11,12 @@
     userName = "rucas";
     userEmail = "lucas.rondenet@gmail.com";
     ignores = [ ".DS_Store" ];
+    aliases = {
+      l =
+        "log --pretty=format:'%h - %an, %an : %s' -n 20 --graph --abbrev-commit";
+      conflicts = "diff --name-only --diff-filter=U";
+      contributors = "shortlog --summary --numbered";
+      dm = "!git branch --merged | grep -v '\\*' | xargs -n 1 git branch -d";
+    };
   };
 }
