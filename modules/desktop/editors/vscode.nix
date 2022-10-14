e3a9b0d2ddd84c5824695100d0a04b0f816b0482
rucas <lucas.rondenet@gmail.com>
2022-10-14 10:47:05 -0700

    fix vscode colortheme

--- modules/desktop/editors/vscode.nix
+++ modules/desktop/editors/vscode.nix
@@ -10,7 +10,7 @@
         sha256 = "sha256-+JU/pwIFmrH8wXqC9hh59iJS22zs7ITYhEwWf676RJU=";
       }];
     userSettings = {
-      "workbench.colorTheme" = "Gruvbox Material Dark Medium";
+      "workbench.colorTheme" = "Gruvbox Material Dark";
       "editor.formatOnSave" = true;
     };
   };
