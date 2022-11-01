823af66149bf650636394ff165fe25b643b15779
rucas <lucas.rondenet@gmail.com>
2022-11-01 16:35:30 -0700

    our thiers git alias

--- modules/cli/git.nix
+++ modules/cli/git.nix
@@ -648,5 +648,24 @@
         printf \"\nMost-active dates, with commit count and %%...\n\"; git log-of-count-and-day | head -7; \
         printf \"\nMost-active files, with churn count\n\"; git churn | head -7; \
       }; f"
+
+      # Ours & Theirs - Easy merging when you know which files you want
+      #
+      # Sometimes during a merge you want to take a file from one side wholesale.
+      #
+      # The following aliases expose the ours and theirs commands which let you
+      # pick a file(s) from the current branch or the merged branch respectively.
+      #
+      #   * git ours - Checkout our version of a file and add it
+      #
+      #   * git theirs - Checkout their version of a file and add it
+      #
+      # N.b. the function is there as hack to get $@ doing
+      # what you would expect it to as a shell user.
+      #
+      # Checkout our version of a file and add it.
+      ours   = !"f() { git checkout --ours   $@ && git add $@; }; f"
+      # Checkout their version of a file and add it.
+      theirs = !"f() { git checkout --theirs $@ && git add $@; }; f"
   '';
 }
