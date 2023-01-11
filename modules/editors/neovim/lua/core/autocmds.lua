33be23be9977dc56db731fd1a99e94e6cff3a0ce
rucas <lucas.rondenet@gmail.com>
2023-01-11 10:37:43 -0800

    <cmd> only for term mappings

--- modules/editors/neovim/lua/core/autocmds.lua
+++ modules/editors/neovim/lua/core/autocmds.lua
@@ -56,9 +56,9 @@ vim.cmd([[
 vim.api.nvim_create_autocmd("TermOpen", {
 	pattern = "term://*toggleterm#*",
 	callback = function()
-		vim.keymap.set("t", "<C-h>", ":wincmd h<CR>", { buffer = 0, silent = true })
-		vim.keymap.set("t", "<C-j>", ":wincmd j<CR>", { buffer = 0, silent = true })
-		vim.keymap.set("t", "<C-k>", ":wincmd k<CR>", { buffer = 0, silent = true })
-		vim.keymap.set("t", "<C-l>", ":wincmd l<CR>", { buffer = 0, silent = true })
+		vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>", { buffer = 0, silent = true })
+		vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>", { buffer = 0, silent = true })
+		vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>", { buffer = 0, silent = true })
+		vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>", { buffer = 0, silent = true })
 	end,
 })
