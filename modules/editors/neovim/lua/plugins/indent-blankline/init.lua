c5fb61dbbf46d86c1e281c75dbb67c7794cec3ec
rucas <lucas.rondenet@gmail.com>
2023-01-13 09:37:04 -0800

    no more eol

--- modules/editors/neovim/lua/plugins/indent-blankline/init.lua
+++ modules/editors/neovim/lua/plugins/indent-blankline/init.lua
@@ -1,6 +1,6 @@
 vim.opt.list = true
 -- vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+-- vim.opt.listchars:append("eol:↴")
 vim.opt.listchars:append("tab:› ")
 vim.opt.listchars:append("trail:•")
 
@@ -25,5 +25,4 @@ require("indent_blankline").setup({
 	show_current_context = true,
 	show_first_indent_level = false,
 	show_trailing_blankline_indent = false,
-	show_end_of_line = true,
 })
