a179cc8ff16c8a2ac1ddadee39eeaad2eb68f018
rucas <lucas.rondenet@gmail.com>
2023-01-19 17:31:08 -0800

    update_in_insert for better feedback?

--- modules/editors/neovim/lua/plugins/nvim-lspconfig/init.lua
+++ modules/editors/neovim/lua/plugins/nvim-lspconfig/init.lua
@@ -1,10 +1,9 @@
 local illuminate = require("illuminate")
 
-
 -- LSP Diagnostics
 vim.diagnostic.config({
 	underline = true,
-	update_in_insert = false,
+	update_in_insert = true,
 	virtual_text = {
 		spacing = 4,
 		prefix = "●",
@@ -19,7 +18,6 @@ local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
-	--vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
