9ec0bf20886309cc43823273b116c130f81c8638
rucas <lucas.rondenet@gmail.com>
2023-01-30 10:30:00 -0800

    yabai cleanup

--- modules/darwin/yabai.nix
+++ modules/darwin/yabai.nix
@@ -1,16 +1,22 @@
 { pkgs, ... }: {
-  services.yabai.enable = true;
-  services.yabai.enableScriptingAddition = true;
-  services.yabai.package = pkgs.yabai;
-  services.yabai.config = {
-    mouse_follows_focus = "on";
-    focus_follows_mouse = "autoraise";
-    layout = "bsp";
-    window_placement = "first_child";
-    top_padding = 12;
-    bottom_padding = 12;
-    left_padding = 12;
-    right_padding = 12;
-    window_gap = 6;
+  services.yabai = {
+    enable = true;
+    enableScriptingAddition = true;
+    package = pkgs.yabai;
+    config = {
+      mouse_follows_focus = "on";
+      focus_follows_mouse = "autoraise";
+      layout = "bsp";
+      window_placement = "first_child";
+      top_padding = 12;
+      bottom_padding = 12;
+      left_padding = 12;
+      right_padding = 12;
+      window_gap = 6;
+    };
+    extraConfig = ''
+      yabai -m rule --add app='System Preferences' manage=off
+      yabai -m rule --add app='Microsoft Outlook' manage=off
+    '';
   };
 }
