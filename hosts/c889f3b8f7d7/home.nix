27843630add6a25dc4d2d5a4043f8496dad2ad2a
rucas <lucas.rondenet@gmail.com>
2023-01-30 10:21:12 -0800

    remove linking of gui apps for spotlight

--- hosts/c889f3b8f7d7/home.nix
+++ hosts/c889f3b8f7d7/home.nix
@@ -18,24 +18,4 @@
 
   home.sessionPath = [ "$HOME/.toolbox/bin" ];
 
-  #home.activation = {
-  #  copyApplications = lib.mkIf pkgs.stdenv.isDarwin (let
-  #    apps = pkgs.buildEnv {
-  #      name = "home-manager-applications";
-  #      paths = config.home.packages;
-  #      pathsToLink = "/Applications";
-  #    };
-  #  in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-  #    baseDir="$HOME/Applications/Home Manager Apps"
-  #    if [ -d "$baseDir" ]; then
-  #      rm -rf "$baseDir"
-  #    fi
-  #    mkdir -p "$baseDir"
-  #    for appFile in ${apps}/Applications/*; do
-  #      target="$baseDir/$(basename "$appFile")"
-  #      $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
-  #      $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
-  #    done
-  #  '');
-  #};
 }
