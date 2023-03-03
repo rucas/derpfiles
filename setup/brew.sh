4053d055ada9267203fe3654a9127fd90c3e066f
rucas <lucas.rondenet@gmail.com>
2023-03-02 16:53:55 -0800

    remove bash setups

--- setup/brew.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "/usr/local/bin/brew" = "$(which brew)" ]; then
-    echo "Homebrew installed...skipping"
-else
-    echo "Installing homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
