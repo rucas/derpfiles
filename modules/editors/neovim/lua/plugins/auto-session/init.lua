60b287ff2dcedcee932ef8007878c5689a46c7d2
rucas <lucas.rondenet@gmail.com>
2023-03-02 16:50:49 -0800

    autosession hooks

--- modules/editors/neovim/lua/plugins/auto-session/init.lua
+++ modules/editors/neovim/lua/plugins/auto-session/init.lua
@@ -4,8 +4,22 @@ local function restore_nvim_tree()
 	nvim_tree.toggle(false, true)
 end
 
+local function close_floating_windows()
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		local config = vim.api.nvim_win_get_config(win)
+		if config.relative ~= "" then
+			vim.api.nvim_win_close(win, false)
+		end
+	end
+end
+
+local function clear_jumps()
+	vim.cmd("clearjumps")
+end
+
 require("auto-session").setup({
 	log_level = "error",
-	post_restore_cmds = { restore_nvim_tree },
+	pre_save_cmds = { close_floating_windows },
+	post_restore_cmds = { clear_jumps, restore_nvim_tree },
 	auto_session_use_git_branch = true,
 })
