eb7061067162015f083a1c6fcdad8d319262b79b
rucas <lucas.rondenet@gmail.com>
2023-03-02 16:51:49 -0800

    trying to cleanup mappings

--- modules/editors/neovim/lua/plugins/nvim-lspconfig/init.lua
+++ modules/editors/neovim/lua/plugins/nvim-lspconfig/init.lua
@@ -24,11 +24,11 @@ end
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+--local opts = { noremap = true, silent = true }
+--vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+--vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+--vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+--vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -47,21 +47,14 @@ local on_attach = function(client, bufnr)
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
 	vim.keymap.set("n", "<space>sh", vim.lsp.buf.signature_help, bufopts)
 	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
 	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
 	vim.keymap.set("n", "<space>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, bufopts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<space>f", function()
+	vim.keymap.set("n", "<space>F", function()
 		vim.lsp.buf.format({
 			filter = function(c)
 				-- apply whatever logic you want (in this example, we'll only use null-ls)
--- modules/editors/neovim/lua/plugins/which-key/init.lua
+++ modules/editors/neovim/lua/plugins/which-key/init.lua
@@ -1,36 +1,31 @@
 local which_key = require("which-key")
-local terms = require("terminals")
 
 which_key.register({
 	["<leader>b"] = {
 		name = "buffers",
-		d = { "<cmd>bp | bd #<cr>", "Soft Delete Buffer (keep window arrangment)" },
-		D = { "<cmd>bd<cr>", "Hard Delete Buffer (remove window)" },
-	},
-	["<leader>c"] = {
-		name = "commands",
-		s = { "<cmd>lua require'plugins.telescope'.command_finder()<cr>", "Commands" },
-		h = { "<cmd>lua require'plugins.telescope'.command_history_finder()<cr>", "Command History" },
-	},
-	["<leader>e"] = {
-		name = "emojis",
-		s = { "<cmd>Telescope symbols<cr>", "Emojis" },
+		d = { "Soft Delete (keep window arrangment)" },
+		D = { "Hard Delete" },
 	},
 	["<leader>f"] = {
 		name = "file",
-		f = { "<cmd>Telescope find_files<cr>", "Find File" },
-		g = { "<cmd>Telescope git_files<cr>", "Git Files" },
-		o = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
-		b = { "<cmd>Telescope buffers<cr>", "Search Files in Buffers" },
+		b = { "File Buffers" },
+		f = { "Find File" },
+		g = { "Git Files" },
+		o = { "Recent Files" },
+		t = { "File Tree" },
 	},
 	["<leader>l"] = {
 		name = "lsp",
-		r = { "<cmd>Telescope lsp_references show_line=false<cr>", "LSP References" },
-		i = { "<cmd>Telescope lsp_implementations<cr>", "LSP Implementations" },
-		d = { "<cmd>Telescope lsp_document_symbols<cr>", "LSP Document Symbols" },
-		t = { "<cmd>Telescope lsp_type_definitions<cr>", "LSP Type Definitions" },
-		w = { "<cmd>Telescope lsp_workspace_symbols<cr>", "LSP Workspace Symbols" },
-		x = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "LSP Dynamic Workspace Symbols" },
+		D = { "Declaration" },
+		c = { "Code Action" },
+		d = { "Definition" },
+		i = { "Implementations" },
+		r = { "References" },
+		n = { "Rename" },
+		t = { "Type Definitions" },
+		s = { "Symbols" },
+		w = { "Workspace Symbols" },
+		x = { "Dynamic Workspace Symbols" },
 	},
 	["<leader>n"] = {
 		name = "neogen",
@@ -41,68 +36,23 @@ which_key.register({
 	},
 	["<leader>p"] = {
 		name = "project",
-		s = { "<cmd>SearchSession<cr>", "Search Project Sessions" },
-		w = { "<cmd>SaveSession<cr>", "Save Project Session" },
-		d = { "<cmd>DeleteSession<cr>", "Delete Project Session" },
-	},
-	["<leader>g"] = {
-		name = "git",
-		c = { "<cmd>Telescope git_commits<cr>", "Git Commits" },
-		b = { "<cmd>Telescope git_branches<cr>", "Git Branches" },
-		B = { "<cmd>Gitsigns toggle_current_line_blame<cr>", "Git Blame" },
-		s = { "<cmd>Telescope git_status<cr>", "Git Status" },
-		u = {
-			function()
-				terms.gitui()
-			end,
-			"Launch Gitui",
-		},
+		s = { "Search Project Sessions" },
+		w = { "Save Project Session" },
 	},
 	["<leader>r"] = {
 		name = "repl",
-		t = { "<cmd>IronRepl<cr>", "Toggle repl" },
-		r = { "<cmd>IronRestart<cr>", "Restart repl" },
-		f = { "<cmd>IronFocus<cr>", "Focus repl" },
-		h = { "<cmd>IronHide<cr>", "Hide repl" },
+		t = { "Toggle repl" },
+		r = { "Restart repl" },
+		f = { "Focus repl" },
+		h = { "Hide repl" },
 	},
-	["<leader>s"] = {
-		name = "search",
-		s = { "<cmd>Telescope grep_string<cr>", "Grep Under Cursor" },
-		g = { "<cmd>Telescope live_grep<cr>", "Grep Search" },
-		h = { "<cmd>Telescope help_tags<cr>", "Search Help Tags" },
-	},
+	["<leader>/"] = { name = "search" },
 	["<leader>t"] = {
-		-- NOTE: load keybinds in gtd config to override g since git wont be used in gtd
-		name = "tree",
-		t = { "<cmd>NvimTreeToggle<cr>", "Open/Close nvim-tree" },
-		f = { "<cmd>NvimTreeFocus<cr>", "Open and Focus on nvim-tree" },
-		F = { "<cmd>NvimTreeFindFile<cr>", "Open nvim-tree to Current File" },
-		s = { "<cmd>AerialToggle<cr>", "Open LSP Document Symbols Tree" },
-		--	e = { "<cmd>Neorg gtd edit<cr>", "Neorg Task Edit" },
-		--	c = { "<cmd>Neorg gtd capture<cr>", "Neorg Task Capture" },
-		--	v = { "<cmd>Neorg gtd views<cr>", "Neorg Task Views" },
+		name = "terminal",
+		g = { "Gitui" },
+		p = { "Procs" },
 	},
-	["<leader>T"] = {
-		name = "Terminal",
-		c = {
-			function()
-				terms.cal()
-			end,
-			"Launch Calendar",
-		},
-		g = {
-			function()
-				terms.gitui()
-			end,
-			"Launch Gitui",
-		},
-		p = {
-			function()
-				terms.procs()
-			end,
-			"Launch Procs",
-		},
-	},
+	["<leader>F"] = { "Format" },
 	["<leader>W"] = {
 		function()
 			vim.cmd("update!")
@@ -110,14 +60,6 @@ which_key.register({
 		"Save",
 	},
 	["<leader>Z"] = { "<cmd>ZenMode<cr>", "Zen Mode" },
-})
-
-which_key.register({
-	["<C-k>"] = { "<cmd>wincmd k<cr>", "Up" },
-	["<C-j>"] = { "<cmd>wincmd j<cr>", "Down" },
-	["<C-h>"] = { "<cmd>wincmd h<cr>", "Left" },
-	["<C-l>"] = { "<cmd>wincmd l<cr>", "Right" },
-	["<C-x>"] = { "<cmd>lua reload_nvim_conf()<cr>", "Reload Configs" },
 })
 
 which_key.setup({
@@ -128,17 +70,19 @@ which_key.setup({
 	},
 	window = {
 		border = "none",
-		margin = { 1, 30, 1, 0 },
+		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+		padding = { 1, 15, 1, 15 },
 	},
 	layout = {
 		align = "left",
+		height = { min = 4, max = 25 }, -- min and max height of the columns
+		width = { min = 20, max = 50 }, -- min and max width of the columns
+		spacing = 30, -- spacing between columns
 	},
 	disable = {
 		buftypes = {},
 		filetypes = {
 			"TelescopePrompt",
-			--"norg",
-			--"neorg",
 		},
 	},
 	show_help = false,
