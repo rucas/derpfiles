c789faef5b7f999f9885aa1d50a25cfe10cfd3e2
rucas <lucas.rondenet@gmail.com>
2023-03-27 09:41:57 -0700

    fzf use better find command for file widget

--- modules/cli/fzf.nix
+++ modules/cli/fzf.nix
@@ -1,10 +1,8 @@
 { ... }: {
   programs.fzf = {
     enable = true;
+    fileWidgetCommand = "fd --type f";
     defaultOptions = [
-      #"--prompt=..."
-      #"--pointer="
-      #"--marker="
       "--color=fg:#a89984,bg:#282828,hl:#d79921"
       "--color=fg+:#ebdbb2,bg+:#282828,hl+:#fabd2f"
       "--color=info:#83a598,prompt:#b8bb26,pointer:#d65d0e"
