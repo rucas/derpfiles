8d0b21f0209f04e4aa1d794b0bfdeec61954864f
rucas <lucas.rondenet@gmail.com>
2023-04-12 10:21:59 -0700

    treesitter max lines check

--- modules/editors/neovim/lua/plugins/nvim-treesitter/init.lua
+++ modules/editors/neovim/lua/plugins/nvim-treesitter/init.lua
@@ -34,21 +34,23 @@ treesitter.setup({
 		},
 		playground = {
 			enable = true,
-			disable = {},
+			disable = function(lang, bufnr)
+				return vim.api.nvim_buf_line_count(bufnr) > 50000
+			end,
 			updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
 			persist_queries = false, -- Whether the query persists across vim sessions
 			keybindings = {
-			  toggle_query_editor = 'o',
-			  toggle_hl_groups = 'i',
-			  toggle_injected_languages = 't',
-			  toggle_anonymous_nodes = 'a',
-			  toggle_language_display = 'I',
-			  focus_language = 'f',
-			  unfocus_language = 'F',
-			  update = 'R',
-			  goto_node = '<cr>',
-			  show_help = '?',
+				toggle_query_editor = "o",
+				toggle_hl_groups = "i",
+				toggle_injected_languages = "t",
+				toggle_anonymous_nodes = "a",
+				toggle_language_display = "I",
+				focus_language = "f",
+				unfocus_language = "F",
+				update = "R",
+				goto_node = "<cr>",
+				show_help = "?",
 			},
-		}
+		},
 	},
 })
