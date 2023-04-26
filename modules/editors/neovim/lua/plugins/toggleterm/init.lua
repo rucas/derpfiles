33b94def3da63d7b3c6edec7baddb53a645c7d27
rucas <lucas.rondenet@gmail.com>
2023-04-26 16:37:25 -0700

    better clear terminal

--- modules/editors/neovim/lua/plugins/toggleterm/init.lua
+++ modules/editors/neovim/lua/plugins/toggleterm/init.lua
@@ -15,7 +15,7 @@ require("toggleterm").setup({
 	on_open = function(terminal)
 		vim.bo.scrollback = 100000
 		set_nvimtree_when_open_term(terminal)
-		vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { buffer = 0, silent = true })
+		--vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { buffer = 0, silent = true })
 		vim.keymap.set("t", "jk", [[<C-\><C-n>]], { buffer = 0, silent = true })
 		vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>", { buffer = 0, silent = true })
 		vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>", { buffer = 0, silent = true })
@@ -23,6 +23,11 @@ require("toggleterm").setup({
 		vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>", { buffer = 0, silent = true })
 		vim.keymap.set("t", "<C-w>", function()
 			vim.fn.feedkeys("\x0c", "n")
+			vim.opt_local.scrollback = 1
+			vim.api.nvim_command("startinsert")
+			vim.api.nvim_feedkeys("clear", "t", false)
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<cr>", true, false, true), "t", true)
+			vim.opt_local.scrollback = 100000
 		end, { buffer = 0, silent = true })
 	end,
 	on_close = function(_) end,
