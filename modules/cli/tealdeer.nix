3aa3beb7406dbdb9554ec18a3cf8a90539d112d1
rucas <lucas.rondenet@gmail.com>
2023-04-27 14:02:33 -0700

    autoupdate set for tealdeer

--- modules/cli/tealdeer.nix
+++ modules/cli/tealdeer.nix
@@ -1,1 +1,12 @@
-{ ... }: { programs.tealdeer = { enable = true; }; }
+{ ... }: {
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      display = {
+        compact = true;
+        use_pager = true;
+      };
+      updates = { auto_update = false; };
+    };
+  };
+}
