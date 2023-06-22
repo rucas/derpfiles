658a9cd3add81da9fa66d25f3bbd282a4466dcf3
rucas <lucas.rondenet@gmail.com>
2023-06-22 16:52:55 -0700

    espanso snippets

--- modules/desktop/productivity/espanso.nix
+++ modules/desktop/productivity/espanso.nix
@@ -5,15 +5,26 @@ let
   inherit (lib.systems.elaborate { system = builtins.currentSystem; })
     isLinux isDarwin;
   settings = {
-    matches = [{
-      trigger = ":date";
-      replace = "{{date}}";
-      vars = [{
-        name = "date";
-        type = "date";
-        params = { format = "%m/%d/%Y"; };
-      }];
-    }];
+    matches = [
+      {
+        trigger = ":date";
+        replace = "{{date}}";
+        vars = [{
+          name = "date";
+          type = "date";
+          params = { format = "%m/%d/%Y"; };
+        }];
+      }
+      {
+        trigger = ":ip";
+        replace = "{{output}}";
+        vars = [{
+          name = "output";
+          type = "shell";
+          params = { cmd = "curl 'https://api.ipify.org'"; };
+        }];
+      }
+    ];
   };
 in mkMerge [
   (optionalAttrs isLinux {
