b700a4e544a0ab86486eaa182e931b1771de0576
rucas <lucas.rondenet@gmail.com>
2023-09-27 17:04:22 -0700

    ssh setup

--- hosts/rucaslab/configuration.nix
+++ hosts/rucaslab/configuration.nix
@@ -99,10 +99,14 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
