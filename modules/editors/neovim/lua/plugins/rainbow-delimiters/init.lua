0abb25794ad8dcc082407e2865135c37764d7f50
rucas <lucas.rondenet@gmail.com>
2023-11-02 11:06:57 -0700

    rainbow-delimeters cleanup

--- modules/editors/neovim/lua/plugins/rainbow-delimiters/init.lua
+++ modules/editors/neovim/lua/plugins/rainbow-delimiters/init.lua
@@ -1,7 +1,7 @@
 -- This module contains a number of default definitions
 local rainbow_delimiters = require("rainbow-delimiters")
 
-require("rainbow-delimiters.setup")({
+require("rainbow-delimiters.setup").setup({
 	strategy = {
 		[""] = rainbow_delimiters.strategy["global"],
 		vim = rainbow_delimiters.strategy["local"],
