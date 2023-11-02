bc0085fdecfaad6fd167e1c57df05ff64cf43f06
rucas <lucas.rondenet@gmail.com>
2023-11-02 11:06:44 -0700

    no markdown prettier

--- modules/editors/neovim/lua/lsp/null-ls.lua
+++ modules/editors/neovim/lua/lsp/null-ls.lua
@@ -9,7 +9,9 @@ local sources = {
 	null_ls.builtins.formatting.black,
 	null_ls.builtins.formatting.isort,
 	null_ls.builtins.formatting.jq,
-	null_ls.builtins.formatting.prettier,
+	null_ls.builtins.formatting.prettier.with({
+		disabled_filetypes = { "markdown" },
+	}),
 
 	null_ls.builtins.diagnostics.flake8,
 	null_ls.builtins.diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
