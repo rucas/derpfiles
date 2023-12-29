dbf201abbe385c76713ee88ea7571cc94b6c9911
rucas <lucas.rondenet@gmail.com>
2023-12-28 19:21:57 -0800

    remove gh workaround

--- modules/cli/gh.nix
+++ modules/cli/gh.nix
@@ -1,9 +1,5 @@
 { ... }: {
   programs.gh = {
     enable = true;
-    settings = {
-      # Workaround for https://github.com/nix-community/home-manager/issues/4744
-      version = 1;
-    };
   };
 }
