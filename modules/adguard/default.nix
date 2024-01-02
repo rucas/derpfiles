75b2889bd2b6369a000768a83d73a0ba60ebae2e
rucas <lucas.rondenet@gmail.com>
2024-01-02 15:10:48 -0800

    adguard dns settings?

--- modules/adguard/default.nix
+++ modules/adguard/default.nix
@@ -10,6 +10,19 @@
       enable = true;
       mutableSettings = false;
       openFirewall = true;
+      settings = {
+        dns = {
+          all_servers = true;
+          bind_hosts = [ "0.0.0.0" ];
+          bootstrap_dns =
+            [ "9.9.9.10" "149.112.112.10" "2620:fe::10" "2620:fe::fe:10" ];
+          ratelimit = 0;
+          enable_dnssec = true;
+          # 50 MBs
+          cache_size = 1024 * 1024 * 50;
+          cache_optimistic = true;
+        };
+      };
     };
   };
 }
