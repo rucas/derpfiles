e092e4cb96a16e96093f1363c16d63416a251b61
rucas <lucas.rondenet@gmail.com>
2024-01-11 10:51:16 -0800

    atuin history_filter

--- modules/cli/atuin.nix
+++ modules/cli/atuin.nix
@@ -1,1 +1,26 @@
-{ ... }: { programs.atuin = { enable = true; }; }
+{ ... }: {
+  # NOTE: need to delete before intial build...
+  # rm -rf ~/.config/atuin/config.toml && darwin-rebuild switch...
+  programs.atuin = {
+    enable = true;
+    settings = {
+      style = "compact";
+      enter_accept = false;
+      history_filter = [
+        "^cr-pull"
+        "^darwin-rebuild"
+        "^nix build"
+        "^tmux"
+        "^cat"
+        "^which"
+        "^cd"
+        "^tldr"
+        "^vi"
+        "^ls"
+        "^g "
+        "^mwinit"
+        "^nvim"
+      ];
+    };
+  };
+}
