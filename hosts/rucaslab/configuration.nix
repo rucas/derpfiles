69deb2dde524f2bdeadff22a1de7c68191d503c3
rucas <lucas.rondenet@gmail.com>
2024-01-26 13:13:57 -0800

    unlockr nixos

--- hosts/rucaslab/configuration.nix
+++ hosts/rucaslab/configuration.nix
@@ -10,6 +10,7 @@
     ../../nixos/adguard
     ../../nixos/ssh
     ../../nixos/tailscale
+    ../../nixos/unlockr
   ];
 
   age.secrets = { tailscale = { file = ./secrets/tailscale.age; }; };
@@ -26,54 +27,6 @@
     "/dev/disk/by-uuid/2da48510-45d3-40c0-ac58-1a10c28c424f";
   boot.initrd.luks.devices."luks-2da48510-45d3-40c0-ac58-1a10c28c424f".keyFile =
     "/crypto_keyfile.bin";
-
-  # NOTE: https://nixos.wiki/wiki/Remote_LUKS_Unlocking
-  # Remote luks unlock
-  boot.initrd.network.enable = true;
-  boot.initrd.network.ssh = {
-    enable = true;
-    port = 22;
-    shell = "/bin/cryptsetup-askpass";
-    authorizedKeys = [
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKCLdEAArRtMhdvIdXKbBE19qhS3R2pL4Ws79d0U3czlAAAAEHNzaDpydWNhc2xhYi5jb20= lucas@rucaslab.com"
-    ];
-    hostKeys = [ "/etc/secrets/initrd/ssh_host_ed25519_key" ];
-  };
-  boot.kernelParams = [ "ip=dhcp" ];
-  # copy your onion folder
-  boot.initrd.secrets = { "/etc/tor/onion/bootup" = /home/lucas/tor/onion; };
-
-  # copy tor to you initrd
-  boot.initrd.extraUtilsCommands = ''
-    copy_bin_and_libs ${pkgs.haveged}/bin/haveged
-    copy_bin_and_libs ${pkgs.tor}/bin/tor
-  '';
-
-  # start tor during boot process
-  boot.initrd.network.postCommands = let
-    torRc = (pkgs.writeText "tor.rc" ''
-      DataDirectory /etc/tor
-      SOCKSPort 127.0.0.1:9050 IsolateDestAddr
-      SOCKSPort 127.0.0.1:9063
-      HiddenServiceDir /etc/tor/onion/bootup
-      HiddenServicePort 22 127.0.0.1:22
-    '');
-  in ''
-    echo "tor: preparing onion folder"
-    # have to do this otherwise tor does not want to start
-    chmod -R 700 /etc/tor
-
-    echo "make sure localhost is up"
-    ip a a 127.0.0.1/8 dev lo
-    ip link set lo up
-
-    echo "haveged: starting haveged"
-    haveged -F &
-
-    echo "tor: starting tor"
-    tor -f ${torRc} --verify-config
-    tor -f ${torRc} &
-  '';
 
   networking.hostName = "rucaslab";
   networking.networkmanager.enable = true;
