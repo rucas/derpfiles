9dab28d0abecb603f91490b0ee4ba926c89f55fb
rucas <lucas.rondenet@gmail.com>
2024-10-24 10:57:27 -0700

    update neovim autosave

--- modules/editors/neovim/lua/plugins/autosave/init.lua
+++ modules/editors/neovim/lua/plugins/autosave/init.lua
@@ -2,11 +2,10 @@ local utils = require("auto-save.utils.data")
 
 require("auto-save").setup({
 	enabled = true,
-	execution_message = { enabled = false },
 	trigger_events = {
 		immediate_save = { "BufLeave", "FocusLost" },
 		defer_save = { "InsertLeave", "TextChanged" },
-		cancel_defered_save = { "InsertEnter" },
+		cancel_deferred_save = { "InsertEnter" },
 	},
 	condition = function(buf)
 		if utils.not_in(vim.bo.filetype, { "gitcommit" }) then
