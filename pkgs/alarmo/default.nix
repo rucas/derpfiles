cf4590e94db176a6477ead7022e818526fd195d4
rucas <lucas.rondenet@gmail.com>
2024-12-26 11:27:52 -0800

    update alarmo

--- pkgs/alarmo/default.nix
+++ pkgs/alarmo/default.nix
@@ -4,12 +4,12 @@ buildHomeAssistantComponent rec {
 
   owner = "nielsfaber";
   domain = "alarmo";
-  version = "1.10.5";
+  version = "1.10.7";
 
   src = fetchFromGitHub {
     owner = owner;
     repo = "alarmo";
     rev = "refs/tags/v${version}";
-    hash = "sha256-Wm4OlyY7ianjD9wg5f7B7VM8/ZaNb4ICR7uKI0KAa1Q=";
+    hash = "sha256-EFR8GveMNpwhrIA0nP+Ny3YUTHAOFw+IF72hH1+wMSM=";
   };
 }
