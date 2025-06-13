ad6f5aff49270b46ea234b66d9b7b6e0df9bbc77
rucas <lucas.rondenet@gmail.com>
2025-06-13 15:29:12 -0700

    switch to working cloudflare dynamic dns

--- nixos/dynamicdns/default.nix
+++ nixos/dynamicdns/default.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
-  services.cfdyndns = {
+  services.cloudflare-dyndns = {
     enable = true;
     apiTokenFile = config.age.secrets.cloudflare_dynamicdns.path;
-    records = [ "portal.rucaslab.com" ];
+    domains = [ "portal.rucaslab.com" ];
   };
 }
