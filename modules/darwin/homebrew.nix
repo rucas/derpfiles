d7317def1576468952a3652f7da90ba493d8d7ed
rucas <lucas.rondenet@gmail.com>
2025-06-18 14:32:58 -0700

    better cask separations

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,31 +1,31 @@
-{ ... }: {
+{ config, ... }:
+let
+  hostSpecificCasks = {
+    "lronden-m-vy79p" = [ "slack" ];
+    "blkmrkt" = [ "balenaetcher" "mqttx" "mqtt-explorer" "tailscale" "vlc" ];
+  };
+
+  commonCasks = [
+    "1password"
+    "aerial"
+    "appcleaner"
+    "blackhole-16ch"
+    "dash"
+    "docker"
+    "dynobase"
+    "espanso"
+    "google-chrome"
+    "maccy"
+    "obs"
+    "shortcat"
+  ];
+in {
   homebrew = {
     enable = true;
     onActivation = { cleanup = "zap"; };
     taps = [ "bradyjoslin/sharewifi" "espanso/espanso" ];
-    casks = [
-      "1password"
-      "aerial"
-      "appcleaner"
-      "balenaetcher"
-      "blackhole-16ch"
-      "dash"
-      "docker"
-      "dropbox"
-      "dynobase"
-      "espanso"
-      "google-chrome"
-      "maccy"
-      "mqttx"
-      "mqtt-explorer"
-      "obs"
-      "obsidian"
-      "shortcat"
-      "sketch"
-      "slack"
-      "tailscale"
-      "vlc"
-    ];
+    casks = commonCasks
+      ++ (hostSpecificCasks.${config.networking.hostName} or [ ]);
     brews = [ "blueutil" "chrome-cli" "openssh" "pyenv" "tor" ];
   };
 
