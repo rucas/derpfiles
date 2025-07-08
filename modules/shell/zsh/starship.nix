162b3f3ca7c6730e475b230cdb00bc0cbb023b49
rucas <lucas.rondenet@gmail.com>
2025-07-08 10:44:56 -0700

    disable git_status for now wait for fsmonitor imp

--- modules/shell/zsh/starship.nix
+++ modules/shell/zsh/starship.nix
@@ -2,9 +2,11 @@
   programs.starship = {
     enable = true;
     settings = {
+      git_status = { disabled = true; };
       git_branch = {
         style = "purple";
         symbol = "";
+        # disabled = true;
       };
       command_timeout = 1000;
     };
