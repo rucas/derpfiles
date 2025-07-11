21492ed8ac3723b351d05bc62d1a03035f964a0c
rucas <lucas.rondenet@gmail.com>
2025-07-10 17:25:21 -0700

    fix deprecated iconUpdateURL

--- modules/desktop/browsers/firefox.nix
+++ modules/desktop/browsers/firefox.nix
@@ -82,7 +82,7 @@ in {
             urls = [{
               template = "https://nixos.wiki/index.php?search={searchTerms}";
             }];
-            iconUpdateURL = "https://nixos.wiki/favicon.png";
+            icon = "https://nixos.wiki/favicon.png";
             updateInterval = 24 * 60 * 60 * 1000; # every day
             definedAliases = [ "@nw" ];
           };
