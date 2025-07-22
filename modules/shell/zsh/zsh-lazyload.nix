abc1a7a86b1603078258f20c06b18652759c2341
rucas <lucas.rondenet@gmail.com>
2025-07-22 10:29:35 -0700

    add pyenv lazy load

--- modules/shell/zsh/zsh-lazyload.nix
+++ modules/shell/zsh/zsh-lazyload.nix
@@ -9,8 +9,10 @@
         sha256 = "sha256-prV6jUp33HgGayf5PuqyM/17/2hJldxMZZskthtdUxY=";
       };
     }];
+    # TODO add check to add only if in brew config
     initContent = ''
       lazyload sdk -- 'export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec && source ''${SDKMAN_DIR}/bin/sdkman-init.sh'
+      lazyload pyenv -- 'eval "$(pyenv init -)"; eval "$(pyenv virtualenv-init -)"'
     '';
   };
 }
