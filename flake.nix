96299b36fe30f6f1e21fb246276de577159b7b1d
rucas <lucas.rondenet@gmail.com>
2025-07-25 15:20:57 -0700

    add gitAlias nix flake

--- flake.nix
+++ flake.nix
@@ -2,8 +2,12 @@
   description = "The original derpfiles flake";
 
   inputs = {
-    agenix = { url = "github:ryantm/agenix"; };
-    nixpkgs = { url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+    agenix = {
+      url = "github:ryantm/agenix";
+    };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,16 +16,28 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    utils = { url = "github:gytis-ivaskevicius/flake-utils-plus"; };
-    alacritty-theme = { url = "github:alexghr/alacritty-theme.nix"; };
-    golink = { url = "github:tailscale/golink"; };
-    neorg-overlay = { url = "github:nvim-neorg/nixpkgs-neorg-overlay"; };
-    neovim-nightly = { url = "github:nix-community/neovim-nightly-overlay"; };
+    utils = {
+      url = "github:gytis-ivaskevicius/flake-utils-plus";
+    };
+    alacritty-theme = {
+      url = "github:alexghr/alacritty-theme.nix";
+    };
+    golink = {
+      url = "github:tailscale/golink";
+    };
+    neorg-overlay = {
+      url = "github:nvim-neorg/nixpkgs-neorg-overlay";
+    };
+    neovim-nightly = {
+      url = "github:nix-community/neovim-nightly-overlay";
+    };
     tmux-1password = {
       url = "github:yardnsm/tmux-1password";
       flake = false;
     };
-    spacebar = { url = "github:cmacrae/spacebar"; };
+    spacebar = {
+      url = "github:cmacrae/spacebar";
+    };
     fast-syntax-highlighting = {
       url = "github:zdharma-continuum/fast-syntax-highlighting";
       flake = false;
@@ -30,7 +46,9 @@
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur = { url = "github:nix-community/nur"; };
+    nur = {
+      url = "github:nix-community/nur";
+    };
     tree-sitter-tera = {
       url = "github:uncenter/tree-sitter-tera";
       flake = false;
@@ -43,45 +61,80 @@
       url = "github:dwyl/english-words";
       flake = false;
     };
+    git-alias = {
+      url = "github:GitAlias/gitalias";
+      flake = false;
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, agenix, nix-darwin, golink, home-manager
-    , utils, spacebar, neovim-nightly, neorg-overlay, alacritty-theme, nur, ...
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      agenix,
+      nix-darwin,
+      golink,
+      home-manager,
+      utils,
+      spacebar,
+      neovim-nightly,
+      neorg-overlay,
+      alacritty-theme,
+      nur,
+      ...
     }:
     let
       inherit (utils.lib) mkFlake;
       inherit (nixpkgs) lib;
       inherit (builtins) fromTOML readFile;
-      mkHost = { host, username, arch, env ? "darwin" }:
+      mkHost =
+        {
+          host,
+          username,
+          arch,
+          env ? "darwin",
+        }:
         let
           isDarwin = if env == "darwin" then true else false;
           isNixOs = !isDarwin;
-        in {
-          builder = {
-            darwin = nix-darwin.lib.darwinSystem;
-            nixos = nixpkgs.lib.nixosSystem;
-          }.${env};
+        in
+        {
+          builder =
+            {
+              darwin = nix-darwin.lib.darwinSystem;
+              nixos = nixpkgs.lib.nixosSystem;
+            }
+            .${env};
           system = arch;
-          specialArgs = { CONF = fromTOML (readFile ./hosts/configs.toml); };
-          modules = [{
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-              theme = fromTOML (readFile ./modules/themes/gruvbox.toml);
-            };
-            home-manager.users.${username} = import ./hosts/${host}/home.nix;
-          }] ++ lib.optionals isDarwin [
-            ./hosts/${host}/darwin.nix
-            home-manager.darwinModules.home-manager
-          ] ++ lib.optionals isNixOs [
-            golink.nixosModules.default
-            ./hosts/${host}/configuration.nix
-            home-manager.nixosModules.home-manager
-            agenix.nixosModules.default
-          ];
-        } // lib.optionalAttrs isDarwin { output = "darwinConfigurations"; };
-    in mkFlake {
+          specialArgs = {
+            CONF = fromTOML (readFile ./hosts/configs.toml);
+          };
+          modules =
+            [
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.extraSpecialArgs = {
+                  inherit inputs;
+                  theme = fromTOML (readFile ./modules/themes/gruvbox.toml);
+                };
+                home-manager.users.${username} = import ./hosts/${host}/home.nix;
+              }
+            ]
+            ++ lib.optionals isDarwin [
+              ./hosts/${host}/darwin.nix
+              home-manager.darwinModules.home-manager
+            ]
+            ++ lib.optionals isNixOs [
+              golink.nixosModules.default
+              ./hosts/${host}/configuration.nix
+              home-manager.nixosModules.home-manager
+              agenix.nixosModules.default
+            ];
+        }
+        // lib.optionalAttrs isDarwin { output = "darwinConfigurations"; };
+    in
+    mkFlake {
       inherit self inputs;
 
       channels.nixpkgs = {
