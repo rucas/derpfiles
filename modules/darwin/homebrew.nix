bd7dacd0e58ea553a8ae50a83fdd27fc50f0c7d5
rucas <lucas.rondenet@gmail.com>
2025-07-28 10:27:14 -0700

    more granular homebrew per hosts

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,44 +1,75 @@
 { config, ... }:
 let
-  hostSpecificCasks = {
-    "salus" = [ "granola" "slack" ];
-    "lronden-m-vy79p" = [ "intellij-idea" "granola" "slack" ];
-    "blkmrkt" = [ "balenaetcher" "mqttx" "mqtt-explorer" "tailscale" "vlc" ];
+  hostSpecific = {
+    brews = {
+      "lronden-m-vy79p" = [
+        "libffi" # needed for pyenv shit
+        "pyenv"
+        "pyenv-virtualenv"
+        "sdkman-cli"
+        "zlib" # needed for pyenv shit
+      ];
+    };
+    casks = {
+      "salus" = [
+        "granola"
+        "slack"
+      ];
+      "lronden-m-vy79p" = [
+        "intellij-idea"
+        "granola"
+        "slack"
+      ];
+      "blkmrkt" = [
+        "balenaetcher"
+        "mqttx"
+        "mqtt-explorer"
+        "tailscale"
+        "vlc"
+      ];
+    };
   };
 
-  commonCasks = [
-    "1password"
-    "aerial"
-    "appcleaner"
-    "blackhole-16ch"
-    "dash"
-    "docker"
-    "dynobase"
-    "espanso"
-    "google-chrome"
-    "insta360-link-controller"
-    "maccy"
-    "obs"
-    "shortcat"
-  ];
-in {
-  homebrew = {
-    enable = true;
-    onActivation = { cleanup = "zap"; };
-    taps = [ "bradyjoslin/sharewifi" "espanso/espanso" "sdkman/tap" ];
-    casks = commonCasks
-      ++ (hostSpecificCasks.${config.networking.hostName} or [ ]);
+  common = {
     brews = [
       "blueutil"
-      "chrome-cli"
       "openssh"
-      "pyenv"
-      "pyenv-virtualenv"
-      "sdkman-cli"
       "tor"
-      "zlib" # needed for pyenv shit
+    ];
+    casks = [
+      "1password"
+      "aerial"
+      "appcleaner"
+      "blackhole-16ch"
+      "dash"
+      "docker"
+      "dynobase"
+      "espanso"
+      "google-chrome"
+      "insta360-link-controller"
+      "maccy"
+      "obs"
+      "shortcat"
+      "spotify"
     ];
   };
+in
+{
+  homebrew = {
+    enable = true;
+    onActivation = {
+      cleanup = "zap";
+    };
+    taps = [
+      "bradyjoslin/sharewifi"
+      "espanso/espanso"
+      "sdkman/tap"
+    ];
+    casks = common.casks ++ (hostSpecific.casks.${config.networking.hostName} or [ ]);
+    brews = common.brews ++ (hostSpecific.brews.${config.networking.hostName} or [ ]);
+  };
 
-  environment.variables = { HOMEBREW_NO_ANALYTICS = "1"; };
+  environment.variables = {
+    HOMEBREW_NO_ANALYTICS = "1";
+  };
 }
