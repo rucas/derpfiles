678851e637498ba0b75e6410e796808803bad0f6
rucas <lucas.rondenet@gmail.com>
2026-02-11 09:07:59 -0800

    adguard host custom user rules as yaml

--- nixos/adguard/default.nix
+++ nixos/adguard/default.nix
@@ -1,14 +1,40 @@
-{ ... }: {
+{
+  pkgs,
+  config,
+  ...
+}:
+let
+  helpers = import ../../lib { inherit pkgs; };
+  inherit (helpers) yamlToAttrs;
+
+  secretRules =
+    let
+      secretPath = ../../secrets + "/adguard-rules-${config.networking.hostName}.yaml";
+    in
+    if builtins.pathExists secretPath then
+      let
+        parsed = yamlToAttrs secretPath;
+      in
+      parsed.user_rules or [ ]
+    else
+      [ ];
+in
+{
   networking = {
     firewall = {
-      allowedTCPPorts = [ 80 443 ];
+      allowedTCPPorts = [
+        80
+        443
+      ];
       allowedUDPPorts = [ 53 ];
     };
   };
   services = {
     caddy = {
       virtualHosts = {
-        "adguard.rucaslab.com" = { extraConfig = "import https-proxy :3000"; };
+        "adguard.rucaslab.com" = {
+          extraConfig = "import https-proxy :3000";
+        };
       };
     };
     adguardhome = {
@@ -37,22 +63,19 @@
         filters = [
           {
             enabled = true;
-            url =
-              "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_1.txt";
             name = "AdGuard DNS filter";
             id = 1;
           }
           {
             enabled = true;
-            url =
-              "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/ultimate.txt";
+            url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/ultimate.txt";
             name = "HaGeZi's Ultimate DNS Blocklist";
             id = 2;
           }
           {
             enabled = true;
-            url =
-              "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts";
+            url = "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling/hosts";
             name = "Steven Black's Hosts List";
             id = 3;
           }
@@ -64,22 +87,19 @@
           }
           {
             enabled = true;
-            url =
-              "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.txt";
+            url = "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/adblock/tif.txt";
             name = "HaGeZi's Threat Intelligence Feeds DNS Blocklist";
             id = 5;
           }
           {
             enabled = true;
-            url =
-              "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareAdGuardHome.txt";
+            url = "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareAdGuardHome.txt";
             name = "💊 Dandelion Sprout's Anti-Malware List";
             id = 6;
           }
           {
             enabled = true;
-            url =
-              "https://adguardteam.github.io/HostlistsRegistry/assets/filter_7.txt";
+            url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_7.txt";
             name = "Perflyst and Dandelion Sprout's Smart-TV Blocklist";
             id = 7;
           }
@@ -90,7 +110,10 @@
             id = 8;
           }
         ];
-        user_rules = [ "@@||app2.cision.com^" "@@||api.my-ip.io^" ];
+        user_rules = secretRules ++ [
+          "@@||app2.cision.com^"
+          "@@||api.my-ip.io^"
+        ];
       };
     };
   };
