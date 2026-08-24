a7de8514f9c39666fb1a387d750fa9ef81ce9681
Lucas Rondenet <lucas.rondenet@gmail.com>
2026-08-23 20:17:09 -0700

    :package:(flake): bump (#67)
    * :package:(flake): bump
    
    Flake lock file updates:
    
    • Updated input 'claude-code-safety-net':
        'github:kenryu42/claude-code-safety-net/9fa3c5b' (2026-06-30)
      → 'github:kenryu42/claude-code-safety-net/ac31f4e' (2026-08-22)
    • Updated input 'firefox-addons':
        'gitlab:rycee/nur-expressions/03db9a2?dir=pkgs/firefox-addons' (2026-08-01)
      → 'gitlab:rycee/nur-expressions/ef61395?dir=pkgs/firefox-addons' (2026-08-22)
    • Updated input 'git-alias':
        'github:GitAlias/gitalias/1e46c48' (2026-05-23)
      → 'github:GitAlias/gitalias/7653169' (2026-08-06)
    • Updated input 'home-manager':
        'github:nix-community/home-manager/bf9ce9f' (2026-07-31)
      → 'github:nix-community/home-manager/ec1a8fd' (2026-08-22)
    • Updated input 'i-have-adhd':
        'github:ayghri/i-have-adhd/07684c4' (2026-07-28)
      → 'github:ayghri/i-have-adhd/b42a45a' (2026-08-21)
    • Updated input 'nix-darwin':
        'github:LnL7/nix-darwin/15abb8c' (2026-07-30)
      → 'github:LnL7/nix-darwin/4cff07d' (2026-08-16)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/f8e81fc' (2026-08-01)
      → 'github:nixos/nixpkgs/391b592' (2026-08-20)
    • Updated input 'nur':
        'github:nix-community/nur/836a554' (2026-08-02)
      → 'github:nix-community/nur/b2d0610' (2026-08-23)
    • Updated input 'nxvm':
        'github:rucas/nxvm/7d914cf' (2026-08-13)
      → 'github:rucas/nxvm/12ede95' (2026-08-23)
    • Updated input 'nxvm/neo-tree':
        'github:nvim-neo-tree/neo-tree.nvim/6679b93' (2026-08-05)
      → 'github:nvim-neo-tree/neo-tree.nvim/5e076e5' (2026-08-19)
    • Updated input 'nxvm/neorg-overlay':
        'github:nvim-neorg/nixpkgs-neorg-overlay/a375383' (2026-08-08)
      → 'github:nvim-neorg/nixpkgs-neorg-overlay/c0114e7' (2026-08-21)
    • Updated input 'nxvm/neorg-overlay/neorg':
        'github:nvim-neorg/neorg/d4dd897' (2026-07-26)
      → 'github:nvim-neorg/neorg/6fda795' (2026-08-14)
    • Updated input 'nxvm/neovim-nightly-overlay':
        'github:nix-community/neovim-nightly-overlay/87a9eec' (2026-08-08)
      → 'github:nix-community/neovim-nightly-overlay/832201f' (2026-08-22)
    • Updated input 'nxvm/neovim-nightly-overlay/neovim-src':
        'github:neovim/neovim/f0a0182' (2026-08-07)
      → 'github:neovim/neovim/6423657' (2026-08-21)
    • Updated input 'nxvm/nixvim':
        'github:nix-community/nixvim/c7be493' (2026-08-03)
      → 'github:nix-community/nixvim/bd46166' (2026-08-21)
    • Updated input 'nxvm/pre-commit-hooks':
        'github:cachix/pre-commit-hooks.nix/43b3c1a' (2026-07-17)
      → 'github:cachix/pre-commit-hooks.nix/809414f' (2026-08-22)
    • Updated input 'opnix':
        'github:brizzbuzz/opnix/35344e1' (2026-05-19)
      → 'github:brizzbuzz/opnix/0ea3a9e' (2026-08-18)
    • Updated input 'pre-commit-hooks':
        'github:cachix/pre-commit-hooks.nix/43b3c1a' (2026-07-17)
      → 'github:cachix/pre-commit-hooks.nix/809414f' (2026-08-22)
    • Updated input 'treefmt-nix':
        'github:numtide/treefmt-nix/d1187f8' (2026-07-29)
      → 'github:numtide/treefmt-nix/27b3b12' (2026-08-16)
    
    * update caddy sha
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- nixos/caddy/default.nix
+++ nixos/caddy/default.nix
@@ -5,7 +5,7 @@
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-        hash = "sha256-pNIRthmPf+J6BPfJ51afBCWt66evnRs1+f9wv09EvK0=";
+        hash = "sha256-F7d4HwM4oCkQrFMr4SFSC0r52ONxY+PW6z5BJawW8Ok=";
       };
       extraConfig = ''
         (access-log) {
